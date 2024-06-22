28eee3fbc030a54fd967248803c258ee70d46408
abel <soyabelca@gmail.com>
2024-06-22 17:53:05 -0500

    configuracion y migracion de la entidad Bodega

---
+++ SistemaInventario.AccesoDatos/Configuracion/BodegaConfiguracion.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Configuracion
+{
+    //Esta interfaz permite definir configuraciones específicas para una entidad(Bodega en este caso) utilizando
+    //Fluent API.
+    public class BodegaConfiguracion: IEntityTypeConfiguration<Bodega>
+    {
+        //utilizamos EntityTypeBuilder<Bodega>(builder) para configurar las propiedades de la entidad Bodega
+        //mediante Fluent API.
+        public void Configure(EntityTypeBuilder<Bodega> builder)
+        {
+            builder.Property(x=>x.Id).IsRequired();
+            builder.Property(x=>x.Nombre).IsRequired().HasMaxLength(60);
+            builder.Property(x=>x.Descripcion).IsRequired().HasMaxLength(100);
+            builder.Property(x=>x.Estado).IsRequired();
+
+        }
+    }
+}
--- SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SistemaInventario.Modelos;
+using System.Data;
+using System.Reflection;
+
 
 namespace SistemaInventario.AccesoDatos.Data
 {
+    //IdentityDbContext: es una extensión de DbContext proporcionada por ASP.NET Core Identity, que incluye configuraciones y funcionalidades específicas para la gestión de usuarios, roles y seguridad en la aplicació
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Bodega> Bodegas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder); //Llamamos al método OnModelCreating de la clase base(IdentityDbContext)
+        //builder: Es una instancia de ModelBuilder, que se utiliza para configurar el modelo de datos de Entity Framework Core.
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        // Este método escanea el ensamblado actual(Assembly.GetExecutingAssembly()) en busca de todas
+        // las clases que implementan IEntityTypeConfiguration<T> y aplica sus configuraciones al modelo.
         }
     }
 }
