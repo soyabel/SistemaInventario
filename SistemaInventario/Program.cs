2d1788651fdecdb2c47b187dd9b980412e46a4bd
abel <soyabelca@gmail.com>
2024-06-23 21:10:59 -0500

    Patron Repositorio

--- SistemaInventario/Program.cs
+++ SistemaInventario/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,7 +16,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
-
+builder.Services.AddScoped<IUnidadTrabajo, UnidadTrabajo>(); //Agregamos el servicio de la unidad de trabajo para que sea accesible en cualquier parte del proyecto
 
 var app = builder.Build();
 
