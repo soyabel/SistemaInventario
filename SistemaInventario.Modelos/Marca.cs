4b0b4b3c32368f77ee0ad3fce1fcc36e65544b34
abel <soyabelca@gmail.com>
2024-06-28 09:59:58 -0500

    Implementación del CRUD Marca

---
+++ SistemaInventario.Modelos/Marca.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Marca
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [MaxLength(60, ErrorMessage = "El nombre debe tener como maximo 60 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La descriptcion es requerida")]
+        [MaxLength(100, ErrorMessage = "La descripcion debe tener como maximo 100 caracteres")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "El estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
