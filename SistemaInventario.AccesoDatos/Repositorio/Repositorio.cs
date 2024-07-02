17454078b1c9c8da183f696867d14596f1a0ca43
abel <soyabelca@gmail.com>
2024-07-01 21:18:15 -0500

    Implementación paginación home

--- SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
+++ SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SistemaInventario.AccesoDatos.Data;
 using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos.Especificaciones;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -60,6 +61,36 @@ namespace SistemaInventario.AccesoDatos.Repositorio
 
             return await query.ToListAsync();
         }
+
+        public PagedList<T> ObtenerTodosPaginado(Parametros parametros, Expression<Func<T, bool>> filtro = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro); //select * from where
+            }
+
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirPro in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirPro);  //ejemplo "Categoria.Marca"
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            return PagedList<T>.ToPagedList(query, parametros.PageNumber, parametros.PageSize);
+        }
+
         public async Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null, 
             string incluirPropiedades = null, bool isTracking = true)
         {
@@ -96,5 +127,7 @@ namespace SistemaInventario.AccesoDatos.Repositorio
         {
             dbSet.RemoveRange(entidad);
         }
+
+        
     }
 }
